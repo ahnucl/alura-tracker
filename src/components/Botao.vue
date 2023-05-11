d2b20b1aa3b1c3889ab8de5e4a9b16201dd23da7
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-05-11 14:58:04 -0300

    Cria componente de botão

---
+++ src/components/Botao.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="button" @click="onClick" :disabled="desabilitado">
+      <span class="icon">
+        <i :class="fullIconName"></i>
+      </span>
+      <span>{{ texto }}</span>
+    </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Botao',
+    props: {
+      icone: {
+        type: String
+      },
+      texto: {
+        type: String
+      },
+      desabilitado: {
+        type: Boolean,
+        default: false
+      }
+    },
+    emits: ['click'],
+    methods: {
+      onClick() {
+        this.$emit('click')
+      }
+    },
+    computed: {
+      fullIconName() {
+        return `fas ${this.icone}`
+      }
+    }
+  })
+
+</script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <Cronometro :tempo-em-segundos="tempoEmSegundos" />
-    <button class="button" @click="iniciar" :disabled="cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-play"></i>
-      </span>
-      <span>play</span>
-    </button>
-    <button class="button" v-on:click="finalizar" :disabled="!cronometroRodando">
-      <span class="icon">
-        <i class="fas fa-stop"></i>
-      </span>
-      <span>stop</span>
-    </button>
+    <Botao 
+      icone="fa-play" 
+      texto="play" 
+      :desabilitado="cronometroRodando"
+      @click="iniciar" 
+    />
+    <Botao 
+      icone="fa-stop" 
+      texto="stop" 
+      :desabilitado="!cronometroRodando"
+      @click="finalizar" 
+    />
+    
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Cronometro from './Cronometro.vue'
+import Botao from './Botao.vue'
 
 export default defineComponent({
   name: 'Temporizador',
   emits: ['aoTemporizadorFinalizado'],
   components: {
-    Cronometro
+    Cronometro, 
+    Botao
   },
   data() {
     return {
